b6b1481b0a9201b5d709894864b84c5d48f2ef26
Eugene Kazakov <eugene.kazakov@webpros.com>
2025-04-02 15:53:09 +0200

    Use swagger codegen for generated classes

--- generate.sh
+++ generate.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-./vendor/bin/jane-openapi generate
+docker run -it --rm \
+  -v $(pwd):/local -w /local/ \
+  swaggerapi/swagger-codegen-cli-v3 generate \
+  -l php \
+  -i v1.47.yaml \
+  -o generated-php \
+  -t templates \
+  -c swagger-config.json
